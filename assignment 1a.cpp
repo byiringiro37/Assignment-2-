83f1b19b967a68c7e433868a3e1c9867876334b0
byiringiro37 <138979671+byiringiro37@users.noreply.github.com>
2023-08-04 15:33:15 +0200

    Add files via upload

---
+++ assignment 1a.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+   int main () {
+   	int i,j,d;
+   	int sumd,sumc=0,sumr=0;
+   	bool boole=false;
+	   cout<<"Input the number of rows and columns:"<<endl;
+   	    cin>>i;
+   	     j=i;
+  
+    vector<vector<int>> m(i, vector<int>(j));
+   
+   	for(int x=0; x<i ;x++){
+   		for(int y=0; y<j; y++) {
+   				cout<<"input elements of row "<<x<<""<<"column "<<y<<":"<<endl;
+   				cin>>m[x][y];
+   			}
+		    }
+		    
+		
+	for(int x=0; x<i ;x++){
+   		for(int y=0; y<j; y++) {
+   			sumr=sumr+m[x][y];
+   		}
+   	}
+
+	 	for(int y=0; y<j; y++) {
+	 		for(int x=0; x<i ;x++){
+	 			sumc=sumc+m[x][y];
+			 }
+		 }
+	 
+	     for(int x=0; x<i ;x++) {
+	     sumd=sumd+m[x][x];
+		 }
+	
+	sumr=sumr/i;
+	sumc=sumc/j;
+		 
+		
+	   
+	   
+	   if(sumd==sumc && sumc==sumr) {
+	   
+	      cout<<"the matrix is a square matrix kbs"<<endl;
+   }
+
+
+   
+   
+   	return 0;
+   }
